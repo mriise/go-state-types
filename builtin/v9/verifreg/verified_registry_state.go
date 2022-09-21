b5d0e019db59366f534c9100ed2a72c34c394994
Geoff Stuart <geoff.vball@gmail.com>
2022-09-21 19:23:36 -0400

    test

--- builtin/v9/verifreg/verified_registry_state.go
+++ builtin/v9/verifreg/verified_registry_state.go
@@ -6,7 +6,6 @@ import (
 
 	addr "github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
@@ -54,24 +53,4 @@ func ConstructState(store adt.Store, rootKeyAddress addr.Address) (*State, error
 		VerifiedClients:          emptyMapCid,
 		RemoveDataCapProposalIDs: emptyMapCid,
 	}, nil
-}
-
-// A verifier who wants to send/agree to a RemoveDataCapRequest should sign a RemoveDataCapProposal and send the signed proposal to the root key holder.
-type RemoveDataCapProposal struct {
-	// VerifiedClient is the client address to remove the DataCap from
-	// The address must be an ID address
-	VerifiedClient addr.Address
-	// DataCapAmount is the amount of DataCap to be removed from the VerifiedClient address
-	DataCapAmount DataCap
-	// RemovalProposalID is the counter of the proposal sent by the Verifier for the VerifiedClient
-	RemovalProposalID RmDcProposalID
-}
-
-// A verifier who wants to submit a request should send their RemoveDataCapRequest to the RKH.
-type RemoveDataCapRequest struct {
-	// Verifier is the verifier address used for VerifierSignature.
-	// The address can be address.SECP256K1 or address.BLS
-	Verifier addr.Address
-	// VerifierSignature is the Verifier's signature over a RemoveDataCapProposal
-	VerifierSignature crypto.Signature
 }
